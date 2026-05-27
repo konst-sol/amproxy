99b837b9c9e9d94fdff146dfbe67c036db2e21b3
Konst Sol <roudken@gmail.com>
2026-05-27 18:37:12 +0300

    обработка ошибок чтения конфиг-файла

--- ctrl.py
+++ ctrl.py
@@ -4,22 +4,23 @@
 import sys, os
 import socket
 import argparse
-from configparser import ConfigParser
+import configparser
 
 sys.dont_write_bytecode = True # чтобы не создавать __pycache__/ в текущем каталоге
 
 def info(*args):
     print(*args, file=sys.stderr)
-error = info
+def error(*args):
+    print(f'{sys.argv[0]}: ERROR:', *args, file=sys.stderr)
 
 class CtrlArgumentParser(argparse.ArgumentParser):
     def error(self, message):
         # Выводим стандартный блок использования (usage)
         self.print_usage(sys.stderr)
         # Выводим текст конкретной ошибки
-        error(f'{self.prog}: error: {message}')
+        error(message)
         # Выводим подсказку
-        error(f'Try "{self.prog} --help" for more information.')
+        info(f'Try "{self.prog} --help" for more information.')
         sys.exit(2)
 
 
@@ -43,18 +44,23 @@ def send_command(command, host, port):
             response = buffer.decode('utf-8')
             print(response) # ответ сервера выводим в stdout
     except socket.timeout:
-        error(f'ERROR: Превышено время ожидания ответа от {host}:{port}')
+        error(f'Превышено время ожидания ответа от {host}:{port}')
         sys.exit(1)
     except ConnectionRefusedError:
-        error(f'ERROR: Не удалось подключиться к {host}:{port}. Прокси-сервер запущен?')
+        error(f'Не удалось подключиться к {host}:{port}. Прокси-сервер запущен?')
         sys.exit(1)
-    except Exception as e:
-        error(f'ERROR: Произошла непредвиденная ошибка: {e}')
+    except Exception as err:
+        error(f'Произошла непредвиденная ошибка: {err}')
         sys.exit(1)
 
 def get_port_from_config(config_path):
-    config = ConfigParser(interpolation=None, inline_comment_prefixes=('#', ';'))
-    config.read((config_path), encoding='utf-8')
+    config = configparser.ConfigParser(interpolation=None,
+                                       inline_comment_prefixes=('#', ';'))
+    try:
+        config.read((config_path), encoding='utf-8')
+    except configparser.Error as err:
+        error(f'Ошибка чтения конфиг-файла: {type(err).__name__}: {err.message}')
+        sys.exit(1)
     for section in config.sections():
         if 'control_port' in config[section]:
             port = config[section]['control_port']
@@ -117,18 +123,18 @@ NB: все служебные сообщения выводятся в stderr, �
         port = args.port
     elif args.config:
         if not os.path.isfile(args.config):
-            error(f'ERROR: конфиг-файл {args.config} не найден')
+            error(f'конфиг-файл {args.config} не найден')
             sys.exit(1)
         port = get_port_from_config(args.config)
         if not port:
-            error(f'ERROR: опция control_port в конфиг-файле {args.config} не найдена')
+            error(f'опция control_port в конфиг-файле {args.config} не найдена')
             sys.exit(1)
         port = int(port)
     else:
         try:
             import amproxy
         except ImportError:
-            error('ERROR: Не удалось импортировать amproxy. '
+            error('Не удалось импортировать amproxy. '
                   'Укажите в командной строке порт управляющего сервера (-p) '
                   'или путь к конфиг-файлу (-c)')
             sys.exit(1)
@@ -137,7 +143,7 @@ NB: все служебные сообщения выводятся в stderr, �
             info(f'Определение порта из {amproxy.CONFIG_PATH}')
             port = amproxy.CONTROL_PORT
             if not port:
-                error('ERROR: Укажите порт управляющего сервера в конфиг-файле '
+                error('Укажите порт управляющего сервера в конфиг-файле '
                       '(например CONTROL_PORT=9999) и перезапустите прокси-сервер')
                 sys.exit(1)
 
